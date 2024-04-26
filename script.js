846393aeadabfa5c91a3214d667a067899806f11
luusytruong <lst27062004@gmail.com>
2024-04-26 17:56:59 +0700

    code completed

---
+++ script.js
@@ -0,0 +1,36 @@
+const tabs = document.querySelectorAll('.list-style.nav');
+const contents = document.querySelectorAll('.container-wrapper');
+const mains = document.querySelectorAll('.main');
+
+tabs.forEach((tab, index) => {
+    const content = contents[index];
+    tab.onclick = function () {
+        const activeTab = document.querySelector('.list-style.active');
+        const activeContent = document.querySelector('.container-wrapper.active');
+        if (activeTab) {
+            activeTab.classList.remove('active');
+        }
+        if (activeContent) {
+            activeContent.classList.remove('active');
+        }
+        this.classList.add('active');
+        content.classList.add('active');
+        console.log(index)
+
+        setTimeout(() => {
+            const widthItem = document.querySelector('.container-wrapper-first').offsetWidth;
+            document.getElementById('scroll').scrollLeft += widthItem;
+        }, 300);
+    }
+});
+
+const closeButtons = document.querySelectorAll('.control-button button');
+
+closeButtons.forEach(button => {
+    button.onclick = function(){
+        setTimeout(() => {
+            const widthItem = document.querySelector('.container-wrapper-first').offsetWidth;
+            document.getElementById('scroll').scrollLeft -= widthItem;
+        }, 100);
+    }
+})
